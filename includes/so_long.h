30d667c9fade8cf5a213330f98e7c9f15a7da769
byaliego <yogun@student.42heilbronn.de>
2022-08-22 18:22:16 +0200

    first commit

---
+++ includes/so_long.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yogun <yogun@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/22 17:53:59 by yogun             #+#    #+#             */
+/*   Updated: 2022/08/22 18:18:49 by yogun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include "mlx/mlx.h"
+# include "libft/libft.h"
+
+#endif
